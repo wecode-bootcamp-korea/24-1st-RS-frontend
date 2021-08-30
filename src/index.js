7da6e0e17f27a9e29c771b5c3500e9028ec222f8
Janine <janine@ddaok-i-pro.local>
2021-08-30 18:24:31 +0900

    ADD: 1차 프로젝트 초기 세팅 수정사항 반영  - 포매터 파일 추가  - reset.scss 적용  - README.md 파일 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-import './style/common.scss';
+
+import './styles/reset.scss';
+import './styles/common.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
