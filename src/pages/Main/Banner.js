47977580be8b9d8e544673bf8f00d70495a4cfb7
ㅇㅅ <es@iuseong-ui-MacBookPro.local>
2021-09-01 18:06:40 +0900

    ADD: Clock 현재시간 시계 구현 완료

--- src/pages/Main/Banner.js
+++ src/pages/Main/Banner.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//import { SliderData } from './BannerSliderData';
 
 export default class Banner extends Component {
   render() {
--- src/pages/Main/BannerSliderData.js
+++ src/pages/Main/BannerSliderData.js
@@ -1,1 +1,22 @@
-import React from 'react';
+export const SliderData = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1493238792000-8113da705763?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1511919884226-fd3cad34687c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1549399542-7e3f8b79c341?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1544636331-e26879cd4d9b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+];
