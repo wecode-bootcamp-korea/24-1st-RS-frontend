5a7de5298675a817e5747a959d08022010dff463
donghee <dongdh02076@gmail.com>
2021-09-01 20:25:05 +0900

    FIX : 공통 스타일링 파일 외 다른 파일 초기화

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
 import './Category.scss';
-import Mainimage from './Mainimage';
-import Maintext from './Maintext';
-import Filter from './Filter';
-import Product from './Product.js';
-import Productlist from './Productlist.js';
 
 export default class Category extends Component {
   render() {
-    return (
-      <div className="category">
-        <div className="nav-bar" />
-        <Mainimage />
-        <Maintext />
-        <div className="category-main-contents">
-          <Filter />
-          <Productlist />
-        </div>
-      </div>
-    );
+    return <div>Hello, Category!</div>;
   }
 }
--- src/pages/Category/Mainimage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Mainimage extends Component {
-  render() {
-    return <div className="main-image" />;
-  }
-}
-
-export default Mainimage;
