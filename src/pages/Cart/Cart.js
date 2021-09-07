29feba3046ee60393b84903bb4263fa8c3bff595
Janine <janine@ddaok-i-pro.local>
2021-09-07 13:42:09 +0900

    FIX: 브랜치 분리 작업  - 상세페이지 기능 외 추가된 파일 삭제

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,44 +1,7 @@
 import React, { Component } from 'react';
-import './Cart.scss';
-
-import Modal from './Modal';
 
 export default class Cart extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isModalOpen: false,
-    };
-  }
-
-  componentDidMount() {
-    fetch('/data/DetailTest2.json')
-      .then(res => res.json())
-      .then(res => this.setState({ infoList: res.Result }));
-  }
-
-  handleModalOpen = () => {
-    this.setState({ isModalOpen: true });
-    console.log(this.state.isModalOpen);
-  };
-
-  handleModalClosed = () => {
-    this.setState({ isModalOpen: false });
-  };
-
   render() {
-    const { isModalOpen } = this.state;
-
-    return (
-      <div>
-        <button onClick={this.handleModalOpen}> 장바구니 보기 </button>
-        <Modal
-          open={isModalOpen}
-          close={this.handleModalClosed}
-          title="Create a chat roozxm"
-        />
-      </div>
-    );
+    return <div></div>;
   }
 }
