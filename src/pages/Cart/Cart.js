6a41a8e08f2631b63b2223d7c377e819e08cb875
Janine <janine@ddaok-i-pro.local>
2021-09-07 13:50:16 +0900

    ADD: 장바구니 레이아웃 형성

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 import './Cart.scss';
 
+import Modal from './Modal';
+
 export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('/data/DetailTest2.json')
+      .then(res => res.json())
+      .then(res => this.setState({ infoList: res.Result }));
+  }
+
+  handleModalOpen = () => {
+    this.setState({ isModalOpen: true });
+    console.log(this.state.isModalOpen);
+  };
+
+  handleModalClosed = () => {
+    this.setState({ isModalOpen: false });
+  };
+
   render() {
-    return <div>주담황</div>;
+    const { isModalOpen } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.handleModalOpen}> 장바구니 보기 </button>
+        <Modal
+          open={isModalOpen}
+          close={this.handleModalClosed}
+          title="Create a chat roozxm"
+        />
+      </div>
+    );
   }
 }
