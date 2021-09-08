97c281ca20b0a87f63114d9ebf48192e32ebca99
donghee <dongdh02076@gmail.com>
2021-09-08 15:06:01 +0900

    ADD : 동적 라우팅 연결, 안주로 검색 기능 추가

--- src/pages/Category/Filter.js
+++ src/pages/Category/Filter.js
@@ -6,17 +6,31 @@ import FilterBtn from './FilterBtn.js';
 class Filter extends Component {
   render() {
     return (
-      <div className="filter">
-        {FILTER_BTN_DATA.map(btn => (
-          <FilterBtn
-            key={btn.id}
-            currentFilter={this.props.filter}
-            filter={btn.filter}
-            text={btn.text}
-            handleFilter={this.props.handleFilter}
-          />
-        ))}
-      </div>
+      <>
+        <div className="filter">
+          {FILTER_BTN_DATA.map(btn => (
+            <FilterBtn
+              key={btn.id}
+              currentFilter={this.props.filter}
+              filter={btn.filter}
+              text={btn.text}
+              handleFilter={this.props.handleFilter}
+            />
+          ))}
+        </div>
+        <input
+          className="filter-input"
+          placeholder="어떤 안주와 즐기고 싶으세요?"
+          name="sideDish"
+          onChange={this.props.handleSideDishInput}
+        />
+        <button
+          className="filter-side-dish-btn"
+          onClick={this.props.handleSideDish}
+        >
+          안주로 검색하기
+        </button>
+      </>
     );
   }
 }
